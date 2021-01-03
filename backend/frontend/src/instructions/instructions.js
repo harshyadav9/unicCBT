7f62b32277305661f6a0cdb77aa121e09945076e
Chestha Yadav <aquachestha@gmail.com>
2021-01-03 23:40:08 +0530

    added frontend module

---
+++ backend/frontend/src/instructions/instructions.js
@@ -0,0 +1,119 @@
+import React, { useState } from 'react'
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
+import Avatar from '@material-ui/core/Avatar';
+import Divider from '@material-ui/core/Divider';
+import ButtonsGroup from '../buttonsGroup/buttonsGroup';
+import Header from '../header/header';
+
+// import image from '../../public/'
+const useStyles = makeStyles((theme) => ({
+    root: {
+        minHeight: '40vh',
+        display: 'flex'
+    },
+    InstructionsDiv: {
+        padding: '9px',
+        boxShadow: '0px 3px 8px 6px #ccc',
+        // lineHeight: '4rem'
+    },
+    root: {
+        flexGrow: 1
+    },
+    img1: {
+        width: '100px',
+        height: '100px'
+    }
+}));
+
+
+function Instructions() {
+    const classes = useStyles();
+    let buttonVal = null;
+    const [questionId, setquestionId] = useState({ id: '', type: '' })
+
+    return (
+        <div className={classes.root}>
+            <Header />
+            <Typography variant="h4" color="textSecondary" align="left">
+                Instructions
+            </Typography>
+            <ButtonsGroup arrLen={200} buttonValue={1} questionInfo={questionId} />
+            <Grid container>
+
+                <Grid item xs={12} md={9} >
+                    <button onClick={() => {
+                        setquestionId({ id: 1, type: 'answered' });
+                    }}>click me </button>
+                    <button onClick={() => {
+                        setquestionId({ id: 2, type: 'answered' });
+                    }}>click me </button>
+                    <button onClick={() => {
+                        setquestionId({ id: 2, type: 'review' });
+                    }}>Review </button>
+                    <button onClick={() => {
+
+                        setquestionId({ id: 2, type: 'unanswered' });
+                    }}>unanswered </button>
+                    {/* <Paper elevation={3}>
+                        <p className={classes.InstructionsDiv}>
+                            <h4>
+                                1. After login, the candidate shall be able to see the detailed instructions for the
+                                examination. Candidates are advised to go through the instructions carefully regarding the type
+                                of questions, marking scheme, procedure to mark & change answer etc. At the designated time
+                                of start of the examination, the candidates will be able to proceed and see the questions on the
+                                computer screen?
+                            </h4>
+                            <br />
+
+                            <h4>
+                                2.  After login, the candidate shall be able to see the detailed instructions for the
+                                examination. Candidates are advised to go through the instructions carefully regarding the type
+                                of questions, marking scheme, procedure to mark & change answer etc. At the designated time
+                                of start of the examination, the candidates will be able to proceed and see the questions on the
+                                computer screen?
+                            </h4>
+
+
+
+                            <br />
+                            <h4>
+                                3.  After login, the candidate shall be able to see the detailed instructions for the
+                                examination. Candidates are advised to go through the instructions carefully regarding the type
+                                of questions, marking scheme, procedure to mark & change answer etc. At the designated time
+                                of start of the examination, the candidates will be able to proceed and see the questions on the
+                                computer screen?
+                            </h4>
+
+                            <br />
+
+                            <h4>
+                                4.  After login, the candidate shall be able to see the detailed instructions for the
+                                examination. Candidates are advised to go through the instructions carefully regarding the type
+                                of questions, marking scheme, procedure to mark & change answer etc. At the designated time
+                                of start of the examination, the candidates will be able to proceed and see the questions on the
+                                computer screen?
+                            </h4>
+
+
+                        </p>
+                    </Paper> */}
+
+                </Grid>
+
+                <Grid item xs={12} md={3} >
+                    <Divider orientation="vertical" flexItem />
+                    {/* <Avatar alt="Remy Sharp" className={classes.img1} src="image1.jpg" />
+                    Name: Harsh Yadav */}
+                </Grid>
+            </Grid>
+
+
+
+        </div>
+    )
+}
+
+export default Instructions
---
+++ backend/frontend/src/login/login.js
@@ -0,0 +1,198 @@
+import React, { useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Footer from '../footer/footer';
+import Header from '../header/header';
+const useStyles = makeStyles((theme) => ({
+
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh'
+  },
+
+  paper: {
+    marginTop: theme.spacing(15),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    boxShadow: 'rgba(0, 0, 0, 0.2) 0px 3px 3px -2px, rgba(0, 0, 0, 0.14) 0px 3px 4px 0px, rgba(0, 0, 0, 0.12) 0px 1px 8px 0px',
+    padding: '3em'
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.dark,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+
+function Copyright() {
+
+  return (
+    <Typography variant="body1" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://www.unicsolutons.edu">
+        www.unicsolutons.edu
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+
+  )
+}
+
+export default function Login() {
+  const classes = useStyles();
+  const [regNo, setregNo] = useState("");
+  const [password, setpassword] = useState("");
+
+  const [regNoErr, setregNoErr] = useState("");
+  const [passwordErr, setpasswordErr] = useState("");
+  const [isregValid, setisRegValid] = useState(false)
+  const [ispassValid, setisPassValid] = useState(false)
+
+
+  const onSubmit = (e) => {
+    console.group(e.value);
+  };
+
+  const regNoChangeEvent = (e) => {
+
+    if (e.target.value == "") {
+      setregNoErr("Empty Registration Number");
+      setisRegValid(false)
+      return;
+    }
+    if (e.target.value.trim() !== "123") {
+      setregNoErr("Incorrect Registration Number");
+      setisRegValid(false)
+      return;
+    }
+    setisRegValid(true)
+    setregNo(e.target.value);
+    setregNoErr("");
+
+
+  };
+
+
+  const submitLoginForm = (e) => {
+    e.preventDefault();
+    console.log(regNo, password);
+  }
+
+  const passChangeEvent = (e) => {
+
+    if (e.target.value == "") {
+      setpasswordErr("Empty Password");
+      setisPassValid(false)
+      return;
+    }
+    if (e.target.value.trim() !== "123") {
+      setpasswordErr("Incorrect Password");
+      setisPassValid(false)
+      return;
+    }
+    setisPassValid(true)
+    setpassword(e.target.value);
+    setpasswordErr("");
+
+
+  };
+
+  return (
+
+
+
+    <div className={classes.root}>
+      {/* header start */}
+      {/* <Header/> */}
+      {/*  header end */}
+      <Container component="div" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+        </Typography>
+
+
+          <form className={classes.form} noValidate>
+            <TextField
+              helperText={regNoErr !== "" ? regNoErr : ""}
+              error={regNoErr !== ""}
+              onChange={regNoChangeEvent}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="regNo"
+              label="Registration Number"
+              name="regNo"
+              autoFocus
+            />
+            <TextField
+              helperText={passwordErr !== "" ? passwordErr : ""}
+              error={passwordErr !== ""}
+              onChange={passChangeEvent}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+            />
+            <Button
+              type="submit"
+              disabled={(isregValid === true && ispassValid === true) === true ? false : true}
+              fullWidth
+              onClick={submitLoginForm}
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+          </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body1">
+                  Forgot password?
+              </Link>
+              </Grid>
+
+            </Grid>
+          </form>
+        </div>
+
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Container>
+      <Footer />
+    </div>
+
+  )
+
+}
