8c84a7e79c2eb3742e5697e426448594132423ec
Chestha Yadav <aquachestha@gmail.com>
2021-06-21 01:15:57 +0530

    fixed null point issue for login

--- backend/src/main/java/com/exam/cbt/service/impl/StudentServiceImpl.java
+++ backend/src/main/java/com/exam/cbt/service/impl/StudentServiceImpl.java
@@ -25,7 +25,7 @@ public class StudentServiceImpl implements StudentService{
 	@Override
 	public String getStudent(Integer registrationNo, String password) {
 		
-		Optional<Student> student =  studentRepository.findByRegistrationNo(registrationNo);
+		Optional<Student> student =  studentRepository.findById(registrationNo);
 		
 		if (student.isPresent()) {
 			if (student.get().getPassword().equals(password)) {
